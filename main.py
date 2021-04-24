1d6a758e2cfb0ab4b170199024c48bd4d7fc0109
Hemanth kumar M <hemanthkumarm@airasia.com>
2021-04-25 01:17:53 +0530

    Add flushall and delete options

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import redis
 import json
 import flask
 import waitress
+import logging
 from flask import render_template
 
 app = flask.Flask(__name__)
 
-r = redis.Redis(host="localhost", port="6379", db=0)
+try:
+    r = redis.Redis(host="localhost", port="6379", db=0)
+except Exception as e:
+    logging.error("Could not connect to redis server", e)
 
 
 @app.route("/getAllKeys", methods=['post'])
@@ -16,8 +20,64 @@ def get_keys_from():
 
 @app.route("/getVal/<key>")
 def get_val_of(key):
+    val = r.get(key)
+    json_val = f"No data found for {key}"
+    try:
+        json_val = json.loads(val)
+    except Exception as ex:
+        print("Invalid Json: ", ex)
+        if val is not None:
+            json_val = str(val, "utf-8")
+
     return app.response_class(
-        response=json.dumps(json.loads(r.get(key)), indent=4, sort_keys=True),
+        response=json.dumps(json_val, indent=4, sort_keys=True),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+@app.route("/purge/<key>")
+def delete_key(key):
+    num_keys = r.delete(key)
+    if num_keys == 0:
+        status = 500
+        message = "Invalid key"
+    else:
+        status = 200
+        message = f"deleted {key}"
+
+    response = {
+        "status": status,
+        "message": message
+    }
+    return app.response_class(
+        response=json.dumps(response),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+@app.route("/purgeAll/")
+def delete():
+    keys = sorted([str(i)[1:].replace('\'', "") for i in r.keys("*")])
+    if len(keys) == 0:
+        status = 500
+        message = "No Keys to delete"
+    else:
+        r.flushall(asynchronous=True)
+        status = 200
+        message = "Deleted all the keys"
+
+    response = {
+        "status": status,
+        "message": message
+    }
+
+    if len(keys) != 0:
+        response["keys"] = keys
+
+    return app.response_class(
+        response=json.dumps(response),
         status=200,
         mimetype='application/json'
     )
@@ -28,7 +88,11 @@ def home():
     global r
     if r is None:
         r = redis.Redis(host="localhost", port="6379", db=0)
-    keys = sorted([str(i)[1:].replace('\'',"") for i in r.keys("*")])
+    try:
+        keys = sorted([str(i)[1:].replace('\'', "") for i in r.keys("*")])
+    except Exception as ex:
+        keys = []
+        logging.error("Could not fetch data from redis server", ex)
     return render_template('home.html', keys=keys)
 
 
